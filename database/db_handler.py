cf55e15b686292b16caf6e5a0e2f165df7d37c1e
gabezolla <gabriel.zolla1998@gmail.com>
2023-06-25 16:02:09 -0300

    update sql methods

--- database/db_handler.py
+++ database/db_handler.py
@@ -1,4 +1,5 @@
 import mysql.connector
+import uuid
 import json
 
 def connect():
@@ -16,30 +17,58 @@ def connect():
         print(f"Error connecting to database: {error}")
         return None
 
+# Notifications
 def saveNotification(type, contact):
     connection = connect()
     if connection:
         try:
             cursor = connection.cursor()
-            sql = "INSERT INTO notifications (type, contact) VALUES (%s, %s)"
-            values = (type, contact)
+            id = str(uuid.uuid4())
+            sql = "INSERT INTO notifications (type, contact) VALUES (%s, %s, %s)"
+            values = (id, type, contact)
             cursor.execute(sql, values)
-            inserted_id = cursor.lastrowid
             connection.commit()
-            print(f"Data successfully saved. Id: {inserted_id}")
+            print(f"Data successfully saved.")
         except mysql.connector.Error as error:
             print(f"Error saving data: {error}")
         finally:
             cursor.close()
             connection.close()
-
-def saveEncoding(name, encoding):
+            
+def getNotifications():
     connection = connect()
     if connection:
         try:
             cursor = connection.cursor()
-            sql = "INSERT INTO encodings (name, encoding) VALUES (%s, %s)"
-            values = (name, encoding)
+            sql = "SELECT * FROM notifications"
+            cursor.execute(sql)
+            results = cursor.fetchall()
+            notifications = []
+            for result in results:
+                notification = {
+                    "id": result[0],
+                    "type": result[1],
+                    "contact": result[2]
+                }
+                notifications.append(notification)
+            return notifications
+        except mysql.connector.Error as error:
+            print(f"Error retrieving notifications: {error}")
+        finally:
+            cursor.close()
+            connection.close()
+    else:
+        print("Failed to connect to the database")
+
+# Encodings
+def saveEncoding(name, encoding):
+    connection = connect()
+    if connection:
+        try:
+            id = str(uuid.uuid4())
+            cursor = connection.cursor()
+            sql = "INSERT INTO encodings (id, name, encoding) VALUES (%s, %s, %s)"
+            values = (id, name, encoding)
             cursor.execute(sql, values)
             connection.commit()
             print("Data successfully saved")
@@ -48,3 +77,47 @@ def saveEncoding(name, encoding):
         finally:
             cursor.close()
             connection.close()
+
+def getEncodings():
+    connection = connect()
+    if connection:
+        try:
+            cursor = connection.cursor()
+            sql = "SELECT name, encoding FROM encodings"
+            cursor.execute(sql)
+            results = cursor.fetchall()
+            names = [result[0] for result in results]
+            encodings = [result[1] for result in results]
+            return names, encodings
+        except mysql.connector.Error as error:
+            print(f"Error retrieving encodings: {error}")
+        finally:
+            cursor.close()
+            connection.close()
+    else:
+        print("Failed to connect to the database")
+
+            
+def deleteEncodings(id):
+    connection = connect()
+    encodings = []
+
+    if connection:
+        try:
+            cursor = connection.cursor()
+            sql = "DELETE FROM encodings WHERE id = %s"
+            values = (id,)
+            cursor.execute(sql, values)
+            connection.commit()
+            cursor.close()
+            connection.close()
+            return {'mensagem': 'Encoding deletado com sucesso'}
+        except mysql.connector.Error as error:
+            print(f"Error retrieving data: {error}")
+        finally:
+            cursor.close()
+            connection.close()
+
+    return encodings
+
+    
